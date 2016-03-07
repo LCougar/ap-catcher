051319647b3a8f0d94bbdc2384cd326692a2f633
Cougar <LCougar@users.noreply.github.com>
2016-03-07 17:05:26 -0300

    Major update on memory usage
    ap-catcher won't allocate the wordlist on memory;

--- ap-catcher.py
+++ ap-catcher.py
@@ -2,7 +2,7 @@
 Code by Cougar - Do not copy without credits
 www.guiadohacker.com.br
 Written using Python 3.4.4 - For Windows
-Release 1.7.5 (February 18, 2016)
+Release 1.8.1 (March 7, 2016)
 
 Wordlist generator based on keywords
 Copyright (C) 2016  Cougar
@@ -46,23 +46,21 @@ def res(nstrings):
 
 def wordcount(nstrings):
 	words = 0
+	total = 0
 	for i in range(1, nstrings + 1):
-		words = words + (math.factorial(nstrings) / math.factorial(nstrings - i))
-	return int(words)
+		words = (math.factorial(nstrings) / math.factorial(nstrings - i))
+		total = total + words
+	return int(total)
 
-def convertBytes(size, is_memory):
+def convertBytes(size):
 	if (size == 0):
 		return '0b'
 	notation = ('b', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB')
 	i = int(math.floor(math.log(size, 1024)))
 	p = math.pow(1024, i)
 	s = round(size/p, 2)
-	warning_handler = ''
-	if i >= 3 and s >= 2 and is_memory == True:
-		print (space(1))
-		warning_handler = '\n\n!Warning! 32-bit systems (x86) can store only 2GB of memory, exceeding the limit will leave the wordlist incomplete!'
-	return '%s %s %s' % (s, notation[i], warning_handler)
-	
+	return '%s %s' % (s, notation[i])
+
 def convertTime(time):
 	floor = 0
 	for i in range(2):
@@ -71,18 +69,13 @@ def convertTime(time):
 			floor = floor + 1
 	notation = ('seconds', 'minutes', 'hours')
 	return '%.2f %s' % (time, notation[floor])
-	
 
-def filesize(strings, nstrings):
+def filesize(strings, nstrings, wcount):
 	nchar = 0
 	for i in range(nstrings):
 		nchar = nchar + len(strings[i])
-	nchar = nchar * wordcount(nstrings)
-	return convertBytes(nchar, False)
-
-def memorysize(strings, nstrings):
-	memory = (sys.getsizeof(strings) * wordcount(nstrings)) / 2
-	return convertBytes(memory, True)
+	nchar = nchar * wcount
+	return convertBytes(nchar)
 
 def zeros(val, nstrings):
 	while len(val) < nstrings:
@@ -100,9 +93,7 @@ def toString(List):
 
 def permute(result, l, r):
 	if l==r:
-		if toString(result) not in lines_seen:
-			wordlist.write(toString(result) + "\n")
-			lines_seen.add(toString(result))
+		wordlist.write(toString(result) + "\n")
 	else:
 		for i in range(l, r + 1):
 			result = swap(result, l, i)
@@ -119,11 +110,11 @@ def checkint():
 		if type(nkeywords) == int:
 			return nkeywords
 
-def parameters():
+def parameters(wcount):
 	print (space(1))
 	print ('Building early structure, %s results.' % (pos))
 	print (space(1))
-	print ('Parameters\nNumber of keywords: %s \nKeywords: %s \nTotal number of words: %s \nApproximate file size: at least %s \nApproximate memory usage: at least %s' % (nkeywords, strings, "{:,}".format(wordcount(nstrings)), filesize(strings, nstrings), memorysize(strings, nstrings)))
+	print ('Parameters\nNumber of keywords: %s \nKeywords: %s \nTotal number of words: %s \nApproximate file size: at least %s' % (nkeywords, strings, "{:,}".format(wcount), filesize(strings, nstrings, wcount)))
 	print (space(1))
 
 # Code start and reload point
@@ -140,7 +131,7 @@ while True:
 	strings = []
 	parlist = []
 	result = []
-	lines_seen = set()
+	storewords = set()
 	wordlist = open('wordlist.txt', "w")
 
 # For user input only
@@ -154,10 +145,11 @@ while True:
 
 	nstrings = len(strings)
 	pos = res(nstrings)
-
+	
 # Wordlist
 
-	parameters()
+	wcount = wordcount(nstrings)
+	parameters(wcount)
 	
 	while True:
 		response = input('Are you sure you want to continue? [y/n]: ')
@@ -196,4 +188,4 @@ wordlist.close()
 
 stop = timeit.default_timer()
 
-print ('Finished with %s results in %s.\nTotal file size: %s \n' % ("{:,}".format(wordcount(nstrings)), convertTime(stop - start), convertBytes(os.path.getsize('wordlist.txt'), False)))
+print ('Finished with %s results in %s.\nTotal file size: %s \n' % ("{:,}".format(wcount), convertTime(stop - start), convertBytes(os.path.getsize('wordlist.txt'))))
